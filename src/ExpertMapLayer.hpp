2f315e8a39ee0355fe7a77a07e9a1ce90fe2eba0
Xanii <73562093+LXanii@users.noreply.github.com>
2024-02-06 20:10:53 -0500

    Update ExpertMapLayer.hpp

--- src/ExpertMapLayer.hpp
+++ src/ExpertMapLayer.hpp
@@ -2,30 +2,105 @@
 
 #include <Geode/DefaultInclude.hpp>
 #include <cocos2d.h>
+#include <Geode/Geode.hpp>
 
 using namespace geode::prelude;
 
+int lives = 30;
+bool super_expert = false;
+
 class ExpertMapLayer : public CCLayer {
+
 protected:
     bool init() override;
     void keyBackClicked() override;
+    
 
 public:
     static ExpertMapLayer* create();
     static ExpertMapLayer* scene();
 
     void onGoBack(CCObject*);
+    void start_expert_run(CCObject*);
+    void end_expert_run(CCObject*);
 };
 
 bool ExpertMapLayer::init() {
     if (!CCLayer::init())
         return false;
+
+    auto director = CCDirector::sharedDirector();
+	auto size = director->getWinSize();
     
     this->setKeypadEnabled(true);
 
-    this->addChild(createLayerBG());
+    CCSprite* expert_run_bg = CCSprite::create("game_bg_08_001.png");
+    CCLabelBMFont* lives_text = CCLabelBMFont::create(std::to_string(lives).c_str(), "gjFont59.fnt");
+    CCSprite* lives_img = CCSprite::create("lives_count.png"_spr);
+    CCLabelBMFont* start_game_text = CCLabelBMFont::create("Start Expert Run", "goldFont.fnt");
+
+    CCMenu* back_btn_menu = CCMenu::create();
+    CCMenu* start_btn_menu = CCMenu::create();
+    CCMenu* end_run_btn_menu = CCMenu::create();
+
+    auto backBtn = CCMenuItemSpriteExtra::create(
+        CCSprite::createWithSpriteFrameName("GJ_arrow_03_001.png"),
+        this, menu_selector(ExpertMapLayer::onGoBack));
+
+    auto startBtn = CCMenuItemSpriteExtra::create(
+        CCSprite::createWithSpriteFrameName("GJ_longBtn03_001.png"),
+        this, menu_selector(ExpertMapLayer::start_expert_run));
+
+    auto endRunBtn = CCMenuItemSpriteExtra::create(
+        CCSprite::createWithSpriteFrameName("GJ_deleteBtn_001.png"),
+        this, menu_selector(ExpertMapLayer::end_expert_run));
+
+    back_btn_menu->setPosition(size.width / 2 - 261, size.height - 30); // exit button position
+    end_run_btn_menu->setPosition({back_btn_menu->getPositionX(), back_btn_menu->getPositionY() - 20});
+
+
+    expert_run_bg->setPosition({size.width / 2, size.height / 2});
+    expert_run_bg->setScale(1.2);
+    expert_run_bg->setColor(ccColor3B(167,15,224));
+
+    end_run_btn_menu->setScale(0.7);
+    end_run_btn_menu->setPosition({end_run_btn_menu->getPositionX() - 85, end_run_btn_menu->getPositionY() - 70});
+
+    lives_img->setScale(0.6);
+    lives_img->setPosition({17,12});
+
+    lives_text->setScale(0.6);
+    lives_text->setPosition({lives_img->getPositionX() + 26,lives_img->getPositionY() - 1.8f});
+
+    start_btn_menu->setPosition({462, lives_text->getPositionY() + 8});
+    start_game_text->setPosition(0,2);
+    start_game_text->setScale(0.68);
+
+    addChild(expert_run_bg, -10); // run first cuz bg thanks everyone
+    addChild(lives_text);
+    addChild(lives_img);
+    addChild(back_btn_menu);
+    back_btn_menu->addChild(backBtn);
+    if (!super_expert) {
+        addChild(start_btn_menu);
+        start_btn_menu->addChild(startBtn);
+        start_btn_menu->addChild(start_game_text);
+    }
+    else {
+        addChild(end_run_btn_menu);
+        end_run_btn_menu->addChild(endRunBtn);
+    }
 
     return true;
+}
+
+void ExpertMapLayer::start_expert_run(CCObject*) {
+    super_expert = true;
+}
+
+void ExpertMapLayer::end_expert_run(CCObject*) {
+    super_expert = false;
+    FLAlertLayer::create("End Run", "Would you like to <cr>end your run</c>?", "OK")->show();
 }
 
 void ExpertMapLayer::keyBackClicked() {
