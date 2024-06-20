b4685f2715742ed0a9a74ae2bbd19f06c32b1fac
Alphalaneous <38200084+Alphalaneous@users.noreply.github.com>
2024-06-20 19:14:26 -0400

    Fix issue

--- src/main.cpp
+++ src/main.cpp
@@ -204,9 +204,10 @@ class $modify(ExpertCallback, CreatorLayer) {
 		m_fields->expertBtnSprite->setScale(0.805);
 
 		m_fields->expertButton = CCMenuItemSpriteExtra::create(m_fields->expertBtnSprite, this, menu_selector(ExpertCallback::onExpert));
+		versusButton->setVisible(false);
 
-		if(Mod::get()->getSettingValue<bool>("hide-versus-button")){
-			versusButton->setVisible(false);
+		if(Mod::get()->getSettingValue<bool>("show-versus-button") && Loader::get()->isModLoaded("alphalaneous.pages_api")){
+			versusButton->setVisible(true);
 		}
 
 		m_fields->expertButton->setPosition({questButton->getPositionX() + 92, questButton->getPositionY() - 2.4f});
